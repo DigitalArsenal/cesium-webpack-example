b4171116cce7dc370b066c675bb89ff22d1bff69
tjkoury <tj@digitalarsenal.io>
2020-09-29 19:01:05 -0400

    fork

--- webpack.release.config.js
+++ webpack.release.config.js
@@ -1,73 +1,26 @@
-const path = require('path');
+const path = require("path");
 
-const webpack = require('webpack');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
+const webpack = require("webpack");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
-module.exports = [{
-    mode: 'production',
+module.exports = [
+  {
+    performance: { hints: false },
+    mode: "production",
     context: __dirname,
     entry: {
-        app: './src/index.js'
+      app: "./src/index.js",
     },
     output: {
-        filename: '[name].js',
-        path: path.resolve(__dirname, 'dist')
+      filename: "[name].js",
+      path: path.resolve(__dirname, "dist"),
     },
-    node: {
-        // Resolve node module use of fs
-        fs: "empty",
-        Buffer: false,
-        http: "empty",
-        https: "empty",
-        zlib: "empty"
-    },
-    resolve: {
-        mainFields: ['module', 'main']
-    },
-    module: {
-        rules: [{
-            test: /\.css$/,
-            use: ['style-loader', { loader: 'css-loader' }],
-            sideEffects: true
-        }, {
-            test: /\.(png|gif|jpg|jpeg|svg|xml|json)$/,
-            use: ['url-loader']
-        }, {
-            // Remove pragmas
-            test: /\.js$/,
-            enforce: 'pre',
-            include: path.resolve(__dirname, 'node_modules/cesium/Source'),
-            sideEffects: false,
-            use: [{
-                loader: 'strip-pragma-loader',
-                options: {
-                    pragmas: {
-                        debug: false
-                    }
-                }
-            }]
-        }]
-    },
-    optimization: {
-        usedExports: true
-    },
+    module: {},
+    optimization: {},
     plugins: [
-        new HtmlWebpackPlugin({
-            template: 'src/index.html'
-        }),
-        // Copy Cesium Assets, Widgets, and Workers to a static directory
-        new CopyWebpackPlugin({
-            patterns: [
-                { from: 'node_modules/cesium/Build/Cesium/Workers', to: 'Workers' },
-                { from: 'node_modules/cesium/Build/Cesium/ThirdParty', to: 'ThirdParty' },
-                { from: 'node_modules/cesium/Build/Cesium/Assets', to: 'Assets' },
-                { from: 'node_modules/cesium/Build/Cesium/Widgets', to: 'Widgets' }
-            ],
-        }),
-        new webpack.DefinePlugin({
-            // Define relative base path in cesium for loading assets
-            CESIUM_BASE_URL: JSON.stringify('')
-        })
-    ]
-}];
+      new HtmlWebpackPlugin({
+        template: "src/index.html",
+      }),
+    ],
+  },
+];
